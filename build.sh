f40c9c069a3b18da2800d049b84ec2edb61801ce
Kevin Papst <kpapst@gmx.net>
2014-04-25 22:52:31 +0200

    added build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+gitbook build . --title="Kimai Documentation" --github=kimai/kimai
+cd _book
+git init
+git commit --allow-empty -m 'updated documentation'
+git checkout -b gh-pages
+touch .nojekyll
+rm build.sh
+
+echo '# GitHub Pages for Kimai docu
+
+Please visit http://kimai.github.io/documentation/ for the online documentation' > README.md
+
+git add .
+git commit -am "updated book"
+git push git@github.com:kimai/documentation gh-pages --force
+
